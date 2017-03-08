ca064ebe1254b5ec42063917bd8b5f9ff6afd805
aravinth4b <aravinth.krishnamoorthi@gmail.com>
2017-03-08 13:28:34 +0530

    added custom service

--- js/custom.js
+++ js/custom.js
@@ -1,20 +1,39 @@
-var app=angular.module("resume",['ui.router']);
-app.controller('Customers',[function(){
-var vm=this;
-vm.title='Customers';
-vm.customers=[{name:'Aravinth'},{name:'London'}];
-}]);
-app.config(function($stateProvider){
-var homestate={
-	name:'home',
-	url:'/home',
-	template: '<h3>Homepage</h3>'
-}
-var aboutstate={
-	name:'about',
-	url:'/about',
-	template:'<h3>About page</h3>'
-}
-$stateProvider.state(homestate);
-$stateProvider.state(aboutstate);
-});
+(function () {
+    'use strict';
+    angular
+    .module("app",['ui.router','ngMessages', 'ngStorage', 'ngMockE2E']);
+    .config(config)
+    .run(run);
+    function config($stateProvider, $urlRouterProvider) {
+        // default route
+        $urlRouterProvider.otherwise("/");
+
+        // app routes
+        $stateProvider
+           
+            .state('login', {
+                url: '/login',
+                templateUrl: 'templates/login.html',
+                controller: 'IndexController',
+                controllerAs: 'vm'
+            });
+    }
+    function run($rootScope, $http, $location, $localStorage) {
+        // keep user logged in after page refresh
+        if ($localStorage.currentUser) {
+            $http.defaults.headers.common.Authorization = 'Bearer ' + $localStorage.currentUser.token;
+        }
+
+        // redirect to login page if not logged in and trying to access a restricted page
+        $rootScope.$on('$locationChangeStart', function (event, next, current) {
+            var publicPages = ['/login'];
+            var restrictedPage = publicPages.indexOf($location.path()) === -1;
+            if (restrictedPage && !$localStorage.currentUser) {
+                $location.path('/login');
+            }
+        });
+    }
+
+})();
+
+
